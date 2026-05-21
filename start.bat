427aa4ad9d04236d31844148c792febc08446f6a
smurfboyyessir <smurfymcsmurfsmurf01@gmail.com>
2026-05-21 13:20:33 -0700

    use nonconflict port

--- start.bat
+++ start.bat
@@ -230,7 +230,7 @@ for /f "usebackq eol=# tokens=1,* delims==" %%A in (".env") do (
 :skip_env
 :: Set defaults only if not already set
 set NODE_ENV=production
-if not defined PORT set PORT=7860
+if not defined PORT set PORT=7861
 if not defined HOST set HOST=0.0.0.0
 
 set PROTOCOL=http
